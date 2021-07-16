cfbc1a66cd42e8f75a4474ad541e0f90bba7a5f8
Naman-12911 <namangarg820@gmail.com>
2021-07-16 19:41:27 +0530

    change in settings

--- iCoder/settings.py
+++ iCoder/settings.py
@@ -137,13 +137,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 # add manual
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static/')
 ]
-#STATIC_ROOT = BASE_DIR / 'static' # to serve static files to heroku
-STATIC_ROOT = os.path.join(BASE_DIR, '/staticfiles')
 
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
